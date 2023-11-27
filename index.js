4c5ea46ea18a663e0ece81110bdbf4f94de64774
Emily Kusich <emilykusich@Emilys-Air.attlocal.net>
2023-11-26 23:32:08 -0500

    corrected get request

--- index.js
+++ index.js
@@ -6,3 +6,17 @@ const fs = require('fs');
 app.use(cors());
 app.use(express.json());
 
+app.get('/', (_req, res) => {
+    const staffData = fs.readFileSync('./data/staff.json');
+    const parsedStaffData = JSON.parse(staffData);
+
+    const newArr = parsedStaffData.map(staff => {
+        return { name: staff.name, color: staff.color }
+    })
+    res.send(staffData)
+}
+)
+
+app.listen(8084, () => {
+    console.log('hey')
+})
