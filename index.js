38452db93c784840ebdba2abeff9a85c08d4022f
Emily Kusich <emilykusich@Emilys-MacBook-Air.local>
2023-11-28 18:53:51 -0500

    added route for table data

--- index.js
+++ index.js
@@ -10,14 +10,23 @@ app.get('/', (_req, res) => {
     const staffData = fs.readFileSync('./data/staff.json');
     const parsedStaffData = JSON.parse(staffData);
 
-    const newArr = parsedStaffData.map(staff => {
+    const staffArr = parsedStaffData.map(staff => {
         return { name: staff.name, color: staff.color }
     })
     res.send(staffData)
 }
 )
 
-app.get('/')
+app.get('/', (_req, res) => {
+    const tablesData = fs.readFileSync('./data/tables.json');
+    const parsedTablesData = JSON.parse(tablesData);
+
+    const tablesArr = parsedTablesData.map(tables => {
+        return { color: tables.color, id: tables.id, width: tables.width, height: tables.height, top: tables.top }
+    })
+    res.send(tablesData)
+}
+)
 
 app.listen(8084, () => {
     console.log('hey')
